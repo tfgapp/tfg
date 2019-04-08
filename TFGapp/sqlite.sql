46f9d385af51240dab7437686d78dc6eb32053d0
gcg9898 <45881205+gcg9898@users.noreply.github.com>
2019-04-08 20:46:36 +0200

    cambio base datos

--- TFGapp/sqlite.sql
+++ TFGapp/sqlite.sql
@@ -12,11 +12,8 @@ INSERT INTO alumnos (nombre,apellido1,apellido2,correo,grado)
 VALUES ( 'Nombre1',Apellido11,Apellido2,Correo1gmail.com,1);
 
 CREATE TABLE IF NOT EXISTS profesores(
-	nombre TEXT,
-	apellido1 TEXT,
-	apellido2 TEXT,
-	correo TEXT PRIMARY KEY NOT NULL,
-	doctor INT
+	nombreCompleto TEXT,
+	octor INT
 );
 
 CREATE TABLE IF NOT EXISTS horarios(
