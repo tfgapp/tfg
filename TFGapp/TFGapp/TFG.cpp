557ff30cfc03cd1840326cce7f506f217e281190
Guillermo Montero <36231066+Unlogix@users.noreply.github.com>
2019-04-12 11:59:49 +0200

    TFG header y cpp

--- TFGapp/TFGapp/TFG.cpp
+++ TFGapp/TFGapp/TFG.cpp
@@ -1,1 +1,25 @@
 #include "TFG.h"
+
+TFG::TFG() {
+
+} //Constructor por defecto
+
+TFG::TFG(string titulo, bool pres) {
+
+} //Constructor con parametros
+
+void TFG::setString(string tit) { //Recibe el valor que queremos dar a Titulo (en este obj.TFG)
+	this->titulo = tit; //Setea el valor de presentado
+}
+
+string TFG::getString() {
+	return titulo;
+}
+
+void TFG::setPresentado(bool pres) { //Recibe el valor que queremos dar a Presentado (en este obj.TFG)
+	this->presentado = pres; //Setea el valor de presentado
+}
+
+bool TFG::getPresentado() {
+	return presentado;
+}
--- TFGapp/TFGapp/TFG.h
+++ TFGapp/TFGapp/TFG.h
@@ -8,9 +8,9 @@ private:
 
 	string titulo;
 	bool presentado;
-
 public:
-
+	TFG(); //Constructor por defecto
+	TFG(string, bool); //Constructor con parametros
 	void setString(string);
 	string getString();
 	void setPresentado(bool);
