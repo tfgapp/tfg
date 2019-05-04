fd50c16601146a5f44cd141e8c0a78702e7dbadd
Ulises <alejandro@gonzalez.ws>
2019-05-04 21:23:43 +0200

    3. Funciona

--- TFGapp/TFGapp/Recursive.cpp
+++ TFGapp/TFGapp/Recursive.cpp
@@ -4,7 +4,7 @@ void prepareR(Controller * main, int convocatoria)
 {
 	//Controller copia_C = (*main);
 	//Seleccionar que alumnos van a ir en esta convocatoria
-	int numAulas = 3;
+	int numAulas = 1;
 	int slots = 7;
 	int dias = main->getDiaMax();
 	int **aulas;
@@ -12,14 +12,18 @@ void prepareR(Controller * main, int convocatoria)
 	for (int i = 0; i < dias; i++) {
 		aulas[i] = new int[slots];
 		for (int j = 0; j < slots; j++) {
-			aulas[i][j] = numAulas;
+			aulas[i][j] = 0;
 		}
 	}
+	aulas[0][1] = 1;
+	aulas[0][2] = 1;
+	aulas[0][3] = 1;
 	vector<Alumno *> convocados;
 
-	for (int i = 0; i < 31; i++) //Utilizo la lista completa de manager para test
+	for (int i = 0; i < 1; i++) //Utilizo la lista completa de manager para test
 	{
 		convocados.push_back(&(*main->getListaAlumnos())[i]);
+		//convocados.push_back(&(*main->getListaAlumnos())[i+1]);
 		convocados.push_back(&(*main->getListaAlumnos())[i + 100]);
 		convocados.push_back(&(*main->getListaAlumnos())[i + 200]);
 		convocados.push_back(&(*main->getListaAlumnos())[i + 300]);
@@ -38,13 +42,13 @@ void prepareR(Controller * main, int convocatoria)
 
 bool RECURSIVE backtracking(Controller * main, vector<Alumno *> *convocados, int convocatoria, int slot, int ** aulas, int dia, int pos)
 {
-	if (!(dia > main->getDiaMax())) cout << "Conv: " << convocatoria << " Slot: " << slot << " Aula: " << aulas[dia - 1][slot] << " Dia: " << dia << " Pos: " << pos << "\n";
-	if (pos >= convocados->size()) return true;
-	if (dia > main->getDiaMax())
+	if (pos >= convocados->size()) return true; //FIN CORRECTO
+	if (dia > main->getDiaMax()) //FIN INCORRECTO
 	{
 		return false;
 	}
-	if (aulas[dia - 1][slot] == 0) { 
+	if (aulas[dia - 1][slot] == 0) //NO QUEDAN AULAS
+	{ 
 		return backtrackingManage(main, convocados, convocatoria, slot + 1, aulas, dia, pos); 
 	}
 	Alumno * alumno = (*convocados)[pos];
@@ -54,62 +58,90 @@ bool RECURSIVE backtracking(Controller * main, vector<Alumno *> *convocados, int
 		return backtracking(main, convocados, convocatoria, slot, aulas, dia, pos + 1);
 	}
 	
-	bool doctor = false; int doctorN = -1;
-	bool extra = false; int extraN = -1;
-	
-	
+	Profesor * profesor = NULL;
 	for (int i = 0; i < main->getListaProfesores()->size(); i++)
 	{
-		Profesor * profesor = &(*main->getListaProfesores())[i];
+		profesor = &(*main->getListaProfesores())[i];
+		if (!profesor->estaEspecializado(alumno->getGrado()) || !profesor->estaDisponible(dia, slot)) continue; //Comprobamos disponibilidad + grado
+		if (!profesor->getDoctor()) continue;
 
-		if (!profesor->estaEspecializado(alumno->getGrado()) || !profesor->estaDisponible(dia, slot)) continue;
-
-		if (profesor->getDoctor() && !doctor)
+		for (int j = 0; j < main->getListaProfesores()->size(); j++)
 		{
-			presentacion->addTribunal(profesor);
-			profesor->getHorario(dia)->setSloot(slot, false);
-			doctor = true; doctorN = i;
+			if (j == i) continue; //Para no coger dos veces el mismo profesor
 
-		}
-		else if(!extra)
-		{
-			extra = true; extraN = i;
-			presentacion->addTribunal(profesor);
-			profesor->getHorario(dia)->setSloot(slot, false);
-		}
+			profesor = &(*main->getListaProfesores())[j];
+			if (!profesor->estaEspecializado(alumno->getGrado()) || !profesor->estaDisponible(dia, slot)) continue;//Comprobamos disponibilidad + grado
 
-		if (doctor && extra)
-		{
+			//Pareja Viable
 			presentacion->setAll(dia, slot, convocatoria);
 			aulas[dia - 1][slot]--;
+			presentacion->addTribunal(&(*main->getListaProfesores())[i]);
+			presentacion->addTribunal(profesor);
+			//if (!(dia > main->getDiaMax())) cout << "Conv: " << convocatoria << " Slot: " << slot << " Aula: " << aulas[dia - 1][slot] << " Dia: " << dia << " Pos: " << pos << "\n";
 			if (backtrackingManage(main, convocados, convocatoria, 0, aulas, 1, pos + 1))
 			{
 				return true;
 			}
-			else 
+			else
 			{
 				aulas[dia - 1][slot]++;
 				presentacion->reset();
-				extra = false;
-				(*main->getListaProfesores())[extraN].getHorario(dia)->setSloot(slot, true);
-				presentacion->delTribunal((*main->getListaProfesores())[extraN].getNombre());
+				while(!presentacion->getTribunal()->empty())
+				{
+					presentacion->getTribunal()->back()->getHorario(dia)->setSloot(slot, true);
+					presentacion->getTribunal()->pop_back();
+				}
 			}
-		}
-		if (i == main->getListaProfesores()->size() - 1 && doctor)
-		{
-			doctor = false;
-			for (int j = 0; j < presentacion->getTribunal()->size(); j++)
-			{
-				presentacion->getTribunal()->back()->getHorario(dia)->setSloot(slot, true);
-				presentacion->getTribunal()->pop_back();
-			}
-			i = doctorN;
 		}
 	}
-	for (int j = 0; j < presentacion->getTribunal()->size(); j++)
+	//for (int i = 0; i < main->getListaProfesores()->size(); i++)
+	//{
+	//	Profesor * profesor = &(*main->getListaProfesores())[i];
+	//	if (!profesor->estaEspecializado(alumno->getGrado()) || !profesor->estaDisponible(dia, slot)) continue;
+	//	if (profesor->getDoctor() && !doctor)
+	//	{
+	//		presentacion->addTribunal(profesor);
+	//		profesor->getHorario(dia)->setSloot(slot, false);
+	//		doctor = true; doctorN = i;
+	//	}
+	//	else if(!extra)
+	//	{
+	//		extra = true; extraN = i;
+	//		presentacion->addTribunal(profesor);
+	//		profesor->getHorario(dia)->setSloot(slot, false);
+	//	}
+	//	if (doctor && extra)
+	//	{
+	//		presentacion->setAll(dia, slot, convocatoria);
+	//		aulas[dia - 1][slot]--;
+	//		if (backtrackingManage(main, convocados, convocatoria, 0, aulas, 1, pos + 1))
+	//		{
+	//			return true;
+	//		}
+	//		else 
+	//		{
+	//			aulas[dia - 1][slot]++;
+	//			presentacion->reset();
+	//			extra = false;
+	//			(*main->getListaProfesores())[extraN].getHorario(dia)->setSloot(slot, true);
+	//			presentacion->delTribunal((*main->getListaProfesores())[extraN].getNombre());
+	//		}
+	//	}
+	//	if (i == main->getListaProfesores()->size() - 1 && doctor)
+	//	{
+	//		doctor = false;
+	//		for (int j = 0; j < presentacion->getTribunal()->size(); j++)
+	//		{
+	//			presentacion->getTribunal()->back()->getHorario(dia)->setSloot(slot, true);
+	//			presentacion->getTribunal()->pop_back();
+	//		}
+	//		i = doctorN;
+	//	}
+	//
+
+	if (pos < 2)
 	{
-		presentacion->getTribunal()->back()->getHorario(dia)->setSloot(slot, true);
-		presentacion->getTribunal()->pop_back();
+		cout << "Pos: "<< pos << " Slot: " << slot << " Aula: " << aulas[dia - 1][slot] << " Dia: " << dia << "\n";
 	}
 	if (backtrackingManage(main, convocados, convocatoria, slot + 1, aulas, dia, pos)) return true;
 	return false;
